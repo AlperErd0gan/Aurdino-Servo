cf2575135c4b32cb14776f30408745ecfab26733
Alper <128478797+AlperErd0gan@users.noreply.github.com>
2024-01-03 20:05:59 +0300

    Update servo_motor.ino

--- servo_motor.ino
+++ servo_motor.ino
@@ -1,14 +1,17 @@
 #include <Servo.h>
-  
+
+// Servo motorlar için nesneler oluşturuluyor
 Servo servo1;
 Servo servo2;
+
+// Joystick pinleri
 int joyX = 0;
 int joyY = 1;
   
 int servoVal;
   
 void setup() 
-{
+{ // Servo motorlar pinlere bağlanıyor
   servo1.attach(3);
   servo2.attach(5);
 }
@@ -16,13 +19,25 @@ void setup()
 void loop()
 {
   
+  // X ekseninden joystick değeri okunuyor
   servoVal = analogRead(joyX);
+  
+  // Okunan değer 0-1023 aralığından 0-180 aralığına alınıyor
   servoVal = map(servoVal, 0, 1023, 0, 180);
+  
+  // Servo motorun pozisyonu belirlenen değere ayarlanıyor
   servo1.write(servoVal);
 
+  // Y ekseninden joystick değeri okunuyor
   servoVal = analogRead(joyY);
+  
+  // Okunan değer 0-1023 aralığından 0-180 aralığına alınyot
   servoVal = map(servoVal, 0, 1023, 0, 180);
+  
+  // Servo motorun pozisyonu belirlenen değere ayarlanıyor
   servo2.write(servoVal);
-  delay(15);  
+
+  // Küçük bir bekleme süresi ekleniyor
+  delay(15);    
 }
 
